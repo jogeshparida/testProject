856ad77db733fd69b2570da9508d8ba8d716bc15
jogeshparida <47854918+jogeshparida@users.noreply.github.com>
2019-02-25 18:43:27 +0530

    Create hello_ti.cpp

---
+++ hello_ti.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <hello_ti_foo.h>
+
+using namespace std;
+
+int main() {
+  cout<<"Welcone to TI"<<endl;
+  // call a function in another file
+  my_foo();
+
+  return 0;
+}
