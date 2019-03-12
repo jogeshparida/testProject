245f15e665e903e63155beeca98ccf60b39af85b
jogeshparida <47854918+jogeshparida@users.noreply.github.com>
2019-03-12 12:02:20 +0530

    Update hello_ti.cpp

--- hello_ti.cpp
+++ hello_ti.cpp
@@ -12,7 +12,6 @@ int main() {
   # Added for Jenkin build TEST - 02
   # Added for Jenkin build TEST - 03
   # Added for Jenkin build TEST - 03
-  # Added for Jenkin build TEST - 04
   my_foo();
 
   return 0;
